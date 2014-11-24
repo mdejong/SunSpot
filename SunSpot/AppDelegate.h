4f798db8f13798c5ee3e027ef12fef5274317676
Mo DeJong <modejong@Mos-MacBook-Air.local>
2014-11-23 17:10:50 -0800

    Initial Commit

---
+++ SunSpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SunSpot
+//
+//  Created by Mo DeJong on 11/23/14.
+//  Copyright (c) 2014 helpurock software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SunSpot/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SunSpot
+//
+//  Created by Mo DeJong on 11/23/14.
+//  Copyright (c) 2014 helpurock software. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
