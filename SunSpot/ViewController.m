d4a14284db62454ea0e835959c8833c4da6a6e2e
Mo DeJong <modejong@Mos-MacBook-Air.local>
2014-11-23 19:32:00 -0800

    first shot at player with playback via movie controller

--- SunSpot/ViewController.m
+++ SunSpot/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
+#import <MediaPlayer/MediaPlayer.h>
+
+#import <AVFoundation/AVFoundation.h>
+
 @interface ViewController ()
+
+@property (nonatomic, retain) MPMoviePlayerController *moviePlayer;
 
 @end
 
@@ -16,12 +22,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *view = self.view;
+    
+    NSString *resourceName = @"SunSpot_1080p_main.m4v";
+    
+    NSString* movieFilePath = [[NSBundle mainBundle] pathForResource:resourceName ofType:nil];
+    NSAssert(movieFilePath, @"movieFilePath is nil");
+    
+    NSURL *fileURL = [NSURL fileURLWithPath:movieFilePath];
+    
+    MPMoviePlayerController *player = [[MPMoviePlayerController alloc] initWithContentURL:fileURL];
+    
+    self.moviePlayer = player;
+    
+    [view addSubview:player.view];
+ 
+    view.autoresizesSubviews = TRUE;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(mediaIsPreparedToPlayDidChange:)
+                                                 name:MPMediaPlaybackIsPreparedToPlayDidChangeNotification
+                                               object:player];
+    
+    [player prepareToPlay];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) mediaIsPreparedToPlayDidChange:(NSNotification*)notification
+{
+    CGRect frame = self.view.frame;
+    
+    NSLog(@"frame size %d, %d", (int)frame.size.width, (int)frame.size.height);
+    
+    self.moviePlayer.view.frame = frame;
+    
+    self.moviePlayer.view.center = self.moviePlayer.view.center;
+    
+    [self.moviePlayer play];
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    CGRect frame = self.view.frame;
+    
+    NSLog(@"frame rotation size %d, %d", (int)frame.size.width, (int)frame.size.height);
+    
+    self.moviePlayer.view.frame = frame;
+    
+    self.moviePlayer.view.center = self.moviePlayer.view.center;
 }
 
 @end
